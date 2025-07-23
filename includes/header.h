7f50b7377fdf527d1cb1674d4d47c4a0cfdf4231
Hermenegildo Kupessa <kupessagil19@gmail.com>
2025-07-23 09:09:47 +0100

    Alteracao da posicao do MAX_P

--- includes/header.h
+++ includes/header.h
@@ -10,25 +10,25 @@
 #include <string.h>
 #include <time.h>
 
-// Defines the prototypes of the functions that 
-// can be called by the header
-
-int	ft_getname (char *name[MAX_P]);
-int	ft_getid (int *id);
-int	ft_getvalue (float *value);
-int	ft_getquantity (int *quantity);
-int	ft_stock (int *quantity);
-int	ft_check_char (char *name[MAX_P]);
-void    ft_start (int id[MAX_P], char name[MAX_P][MAX_CHR], float valeu[MAX_P], int quantity[MAX_P])
-int	ft_search (char *name[MAX_P]);
-int	ft_insert (int id[MAX_P], char *name[MAX_P], float value[MAX_P], int quantity[MAX_P]);
-// Defines the Max product that will be stored
-
-int	MAX_P = 10;
+// Defines the max product stored
+# define MAX_P 10
 
 // Define the Max charactere suported by a string
 
 # define MAX_CHR 100
+
+// Defines the prototypes of the functions that 
+// can be called by the header
+
+int	ft_getname (char *name[MAX_P]);
+int	ft_getid (int *id[MAX_P]);
+int	ft_getvalue (float *value[MAX_P]);
+int	ft_getquantity (int *quantity[MAX_P]);
+int	ft_stock (int *quantity[MAX_P]);
+int	ft_check_char (char *name[MAX_P]);
+void    ft_start (int id[MAX_P], char name[MAX_P][MAX_CHR], float valeu[MAX_P], int quantity[MAX_P]);
+int	ft_search (char *name[MAX_P]);
+int	ft_insert (int id[MAX_P], char *name[MAX_P], float value[MAX_P], int quantity[MAX_P]);
 
 // Integer to show the actual position
 
