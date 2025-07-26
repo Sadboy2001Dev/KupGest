98675b3009ea76bdfa81de4fdbd0786d13997de9
Hermenegildo Kupessa <kupessagil19@gmail.com>
2025-07-26 18:36:48 +0100

    header actualizado com ft_print e menu

--- includes/header.h
+++ includes/header.h
@@ -24,14 +24,16 @@
 // can be called by the header
 
 int	ft_getname (char name[MAX_P][MAX_CHR]);
-int	ft_getid (int id[MAX_P]);
+int	ft_getid (int id[posit]);
 int	ft_getvalue (float value[MAX_P]);
 int	ft_getquantity (int quantity[MAX_P]);
 int	ft_stock (int *quantity[MAX_P]);
 int	ft_check_char (char name[MAX_P][MAX_CHR]);
 void    ft_start (int id[MAX_P], char name[MAX_P][MAX_CHR], float value[MAX_P], int quamtity[MAX_P]);
 int	ft_search (char name[MAX_P][MAX_CHR]);
-int	ft_insert (int id[MAX_P], char name[MAX_P][MAX_CHR], float value[MAX_P], int quantity[MAX_P]);
+int	ft_insert (int id[posit], char name[MAX_P][MAX_CHR], float value[MAX_P], int quantity[MAX_P]);
+void	ft_print (int id[MAX_P], char name[MAX_P][MAX_CHR], float value[MAX_P], int quantity[MAX_P]);
+int	menu (int id[posit], char name[posit][MAX_CHR], float value[posit], int quantity[posit]);
 
 // Defines the struct that will be used
 
