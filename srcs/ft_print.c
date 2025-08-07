8479af315fe807eb7993ebbd400ccb4213c543a0
Hermenegildo Kupessa <kupessagil19@gmail.com>
2025-08-07 12:43:17 +0100

    Actualização do design da saida/apresentação

--- srcs/ft_print.c
+++ srcs/ft_print.c
@@ -5,11 +5,14 @@ void	ft_print (int id[MAX_P], char name[MAX_P][MAX_CHR], float value[MAX_P], int
 	int	i;
 
 	i = 0;
-	printf ("ID\t STOCK\t\t PREÇO\t\t PRODUCTO\n");
+	printf ("ID\t STOCK\t \tPREÇO\t\t PRODUCTO\n");
 	printf ("----------------------------------------------------\n");
 	while (i < MAX_P)
 	{
-		printf ("%d\t %3d\t %12.2f\t\t %s\n", id[i], quantity[i], value[i], name[i]);
+		if (strcmp (name[i], "EMPTY"))
+		{
+			printf ("%d\t %3d\t %12.2f\t\t %s\n", id[i], quantity[i], value[i], name[i]);
+		}
 		i++;
 	}
 	puts ("----------------------------------------------------\n");
