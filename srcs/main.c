de69d79264bfbbd5076f574a75d541c4ab07ef80
Hermenegildo Kupessa <kupessagil19@gmail.com>
2025-08-07 12:44:59 +0100

    Adição da função start

--- srcs/main.c
+++ srcs/main.c
@@ -6,18 +6,9 @@ char	nome[MAX_P][MAX_CHR];
 int	id[MAX_P];
 int	quantidade[MAX_P];
 float	valor[MAX_P];
-int	pos = 0;
 
+// Linker (main)
 int	main (void) {
-
-	// Booting
 	ft_start (id, nome, valor, quantidade);
-/*	ft_insert(&id[pos], &nome[pos], &valor[pos], &quantidade[pos]);
-	ft_print (&id[pos], &nome[pos], &valor[pos], &quantidade[pos]);*/
-	menu (); /*(&id[pos], &nome[pos], &valor[pos], &quantidade[pos]);*/
-/*	if (ft_getid (&id[pos]))
-		if (ft_getquantity (&quantidade[pos]))
-			if (ft_getvalue(&valor[pos]))
-				if (ft_getname(&nome[pos])) printf ("%d %d %.2f %s\n", id[pos], quantidade[pos], valor[pos], nome[0]);
-	pos++;*/
+	menu ();
 }
