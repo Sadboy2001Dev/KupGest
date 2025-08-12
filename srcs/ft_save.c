a81ff0fa1ef642a2d7a2a83f08adcc3e0cb97722
Hermenegildo Kupessa <kupessagil19@gmail.com>
2025-08-12 09:45:24 +0100

    Adicionado função para ler arquivo(s)

--- srcs/ft_save.c
+++ srcs/ft_save.c
@@ -14,7 +14,7 @@ int	ft_save (void)
 //	strcat (filename,".pdf");
 	// Creating the file where the
 	// data will be saved
-	if ((fp = fopen (filename, "a+")) == NULL)
+	if ((fp = fopen (filename, "wb+")) == NULL)
 	{
 		puts ("\nArquivo não pode ser salvo!\n");
 		// Return zero when
@@ -23,24 +23,55 @@ int	ft_save (void)
 	}
 	else
 		puts ("\nArquivo salvo com sucesso!\n");
-	fprintf (fp, "\nID\t STOCK\t \tPREÇO\t\t PRODUCTO\n");
-	fprintf (fp, "----------------------------------------------------\n");
+/*	fprintf (fp, "\nID\t STOCK\t \tPREÇO\t\t PRODUCTO\n");
+	fprintf (fp, "----------------------------------------------------\n");*/
 	while (i < MAX_P)
 	{
 		// Putting the data in the archive
 		if (id[i] != -1)
-			fprintf (fp,"%d\t %3d\t %12.2f\t\t %s\n", id[i], quantidade[i], valor[i], nome[i]);
+		{
+			fwrite (&id[i], sizeof(int), 1, fp);
+			fwrite (&quantidade[i], sizeof (float), 1, fp);
+			fwrite (&valor[i], sizeof (int), 1, fp);
+			fwrite (nome[i], MAX_CHR, 1, fp);
+		}
+		//	fprintf (fp,"%d\t %3d\t %12.2f\t\t %s\n", id[i], quantidade[i], valor[i], nome[i]);
 		i++;
 	}
 	// Closing the opened file
 	fclose (fp);
 	// Return one when everythings goes well
-	return 1;
+	return menu ();
 }
 
 // Function to load the saved data
 
 int	ft_load (void)
 {
+	FILE	*fp;
+	char	filename [MAX_CHR];
+	int	i;
+
+	i = 0;
+	printf ("Nome do ficheiro: ");
+	scanf (" %[^\n]s", filename);
+	if ((fp = fopen (filename, "rb")) != NULL)
+	{
+		puts ("\nFicheiro aberto com sucesso!\n");
+		while (i < MAX_P)                                     {
+			// Getting the data
+			// in the archive
+			fread (&id[i], sizeof(int), 1, fp);
+			fread (&quantidade[i], sizeof (float), 1, fp);
+			fread (&valor[i], sizeof (int), 1, fp);
+			fread (nome[i], MAX_CHR, 1, fp);
+			i++;
+		}
+		fclose (fp);
+		return menu ();
+	}
+	else
+		puts ("\nErro ao abrir o ficheiro\n");
+	//      fprintf (fp,"%d\t %3d\t %12.2f\t\t %s\n", id[i], quantidade[i], valor[i], nome[i]);                 i++;                                          }
 	return 0;
 }
